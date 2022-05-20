851a5ad575f3370a0031b7ac65ae7e2f964d9dd9
vikipretium <vighneshpasupathy@gmail.com>
2022-05-20 15:16:26 +0530

    create person

---
+++ persons.rb
@@ -0,0 +1,28 @@
+class Person
+
+    # constructors and instance vars
+        def initialize(name = 'Unknown', age, parent_permission = true)
+        @id = Random.rand(1..100)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission            
+        end
+    
+    # getters and setter
+        attr_reader :id
+        attr_accessor : name, :age
+    
+    #private method is_of_age?
+
+        def is_of_age?
+            return true if @age >= 18
+            false
+        end
+        private :is_of_age?
+        
+    #public method can_use_services?
+        def can_use_services?
+            return true if is_of_age? || parent_permission == true
+            false
+        end
+    end
