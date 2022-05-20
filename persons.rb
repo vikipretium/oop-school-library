f1570a13c889fa67cb66f53788a7e470a1f825ff
vikipretium <vighneshpasupathy@gmail.com>
2022-05-20 15:44:25 +0530

    fix linters

--- persons.rb
+++ persons.rb
@@ -1,28 +1,29 @@
 class Person
+  # constructors and instance vars
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    # constructors and instance vars
-        def initialize(name = 'Unknown', age, parent_permission = true)
-        @id = Random.rand(1..100)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission            
-        end
-    
-    # getters and setter
-        attr_reader :id
-        attr_accessor : name, :age
-    
-    #private method is_of_age?
+  # getters and setter
+  attr_reader :id
+  attr_accessor :name, :age
 
-        def is_of_age?
-            return true if @age >= 18
-            false
-        end
-        private :is_of_age?
-        
-    #public method can_use_services?
-        def can_use_services?
-            return true if is_of_age? || parent_permission == true
-            false
-        end
-    end
+  # private method is_of_age?
+
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
+  private :of_age?
+
+  # public method can_use_services?
+  def can_use_services?
+    return true if is_of_age? || parent_permission == true
+
+    false
+  end
+end
--- teacher.rb
+++ teacher.rb
@@ -2,14 +2,14 @@
 require './person'
 
 class Teacher < Person
-# constructor specilization
-def initialize(age,specilization, name='Unknown', parent_permission :true)
+  # constructor specilization
+  def initialize(age, specilization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specilization = specilization
-end
+  end
 
-# override can_use_services?
-def can_use_services?
+  # override can_use_services?
+  def can_use_services?
     true
-end
+  end
 end
